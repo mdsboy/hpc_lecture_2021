b0d9ff81f8dfe77ad0967ba4bc00913218b9e905
Yu Daiki <daiki.y.aa@m.titech.ac.jp>
2021-05-28 00:06:44 +0900

    Swapping loop order + OpenMP

--- 16_final_report/example.cpp
+++ 16_final_report/example.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <vector>
 #include <chrono>
+#include <omp.h>
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -38,10 +39,13 @@ int main(int argc, char** argv) {
   for(int irank=0; irank<size; irank++) {
     auto tic = chrono::steady_clock::now();
     offset = N/size*((rank+irank) % size);
+
+#pragma omp parallel for
     for (int i=0; i<N/size; i++)
-      for (int j=0; j<N/size; j++)
-        for (int k=0; k<N; k++)
+      for (int k=0; k<N; k++)
+        for (int j=0; j<N/size; j++)
           subC[N*i+j+offset] += subA[N*i+k] * subB[N/size*k+j];
+
     auto toc = chrono::steady_clock::now();
     comp_time += chrono::duration<double>(toc - tic).count();
     MPI_Request request[2];
