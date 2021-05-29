e352f96a2e98b5c20bfd4625ee50fa4bae924f1b
Yu Daiki <daiki.y.aa@m.titech.ac.jp>
2021-05-29 18:01:43 +0900

    SIMD

--- 16_final_report/example.cpp
+++ 16_final_report/example.cpp
@@ -11,6 +11,7 @@
 #include <vector>
 #include <chrono>
 #include <omp.h>
+#include <immintrin.h>
 using namespace std;
 
 int main(int argc, char** argv) {
@@ -78,8 +79,12 @@ int main(int argc, char** argv) {
             for (int ir=0; ir<mc; ir+=mr) {
               for (int kr=0; kr<kc; kr++) {
                 for (int i=ir; i<ir+mr; i++) {
-                  for (int j=jr; j<jr+nr; j++) { 
-                    Cc[i*nc+j] += Ac[i*kc+kr] * Bc[kr*nc+j];
+                  __m256 Avec = _mm256_broadcast_ss(Ac+i*kc+kr);
+                  for (int j=jr; j<jr+nr; j+=8) {
+                    __m256 Bvec = _mm256_load_ps(Bc+kr*nc+j);
+                    __m256 Cvec = _mm256_load_ps(Cc+i*nc+j);
+                    Cvec = _mm256_fmadd_ps(Avec, Bvec, Cvec);
+                    _mm256_store_ps(Cc+i*nc+j, Cvec);
                   }
                 }
               }
